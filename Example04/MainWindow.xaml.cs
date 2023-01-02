de3d6970dbbe27a1fae0e9fea60267d54e6933a3
Luiz Parente <lp.parente@gmail.com>
2023-01-02 12:49:11 -0500

    Adding more examples.

---
+++ Example04/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example04 {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void OnClickOK(object sender, EventArgs e) {
+			// Named controls can be accessed in the code behind like any other class field.
+			// Since txtName is a TextBox, we can retrieve its current value from property 'Text'.
+			string name = txtName.Text;
+
+			if (string.IsNullOrWhiteSpace(name)) {
+				MessageBox.Show($"Make sure to input your name before clicking the button.");
+			}
+			else {
+				MessageBox.Show($"Hello, {name}!");
+			}
+		}
+	}
+}
